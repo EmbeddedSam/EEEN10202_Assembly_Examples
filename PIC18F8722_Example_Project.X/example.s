ccb7bad1720cce99e6da2ede44ac510f64cc2f59
Sam Walsh <samuel.walsh@manchester.ac.uk>
2024-01-26 14:44:00 +0000

    Added the example project

---
+++ PIC18F8722_Example_Project.X/example.s
@@ -0,0 +1,31 @@
+; Program to move 3 values to 3 memory 
+    
+processor 18F8722
+radix   dec ; use decimal numbers
+    
+CONFIG  OSC = HS  ; use the high speed external crystal on the PCB
+CONFIG  WDT = OFF ; turn off the watchdog timer
+CONFIG  LVP = OFF ; turn off low voltage mode
+
+;Include useful processor-specific definitions
+#include <xc.inc>      
+
+PSECT resetVector, class=CODE, reloc=2
+resetVector:
+    goto start ; On reset, jump to our program start location
+
+; Our program start point
+PSECT start, class=CODE, reloc=2
+start: 
+    movlw 22
+    movwf 10
+    movlw 00101001B
+    movwf 11
+    movlw 0x15
+    movwf 12
+loop:
+    bra loop ; loop back to start
+    end
+
+
+
