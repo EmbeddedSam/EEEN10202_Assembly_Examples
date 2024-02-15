690347d2a991bdea465e8acbb130cea984e9714f
Sam Walsh <samuel.walsh@manchester.ac.uk>
2024-02-15 15:27:17 +0000

    gone back to previous DFP

---
+++ Lecture3_Examples.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Lecture3_Examples.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lecture3_Examples.X.production.hex
---
+++ PIC18F8722_Example_Project.X/build/default/production/example.d
@@ -0,0 +1,1 @@
+build/default/production\example.i: example.s
